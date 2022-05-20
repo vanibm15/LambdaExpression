98c2110acee3dca40ada800d58ea1c78dda25ec5
vanibm15 <vanibm15@gmail.com>
2022-05-20 19:10:18 +0530

    added:passwordrule3

--- src/com/bridgelabz/LambdaExpression.java
+++ src/com/bridgelabz/LambdaExpression.java
@@ -3,20 +3,20 @@ package com.bridgelabz;
 import java.util.regex.Pattern;
 
     @FunctionalInterface
-    interface PasswordRule2 {
+    interface PasswordRule3 {
         void validUser(String password);
     }
 
 public class LambdaExpression {
 
     public static void main(String[] args) {
-        PasswordRule2 function = (password)->{
-            boolean check = Pattern.matches("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]){1,}(?=.*[*.!@$%^&]).{8,}$",password);
+        PasswordRule3 function = (password)->{
+            boolean check = Pattern.matches("^(?=.*[0-9]{1,})(?=.*[a-z])(?=.*[A-Z]){1,}(?=.*[*.!@$%^&]).{8,}$",password);
             if (check == true) {
                 System.out.println("Password is Valid");
             }else
-                System.out.println("Enter valid password");
+                System.out.println("Password Is Invalid");
         };
-        function.validUser("Vani@123");
+        function.validUser("Vani@1234");
     }
 }
